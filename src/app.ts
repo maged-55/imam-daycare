0697f5396a56b2bd49a72597f5eb3a532ef17742
majed <m.a.a.almaged@gmail.com>
2022-11-20 13:00:35 +0300

    add search

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-import {Ipeople} from "./types/people";
+import {Ipeople,Itask,Igrade} from "./types/people";
 import   { NextFunction, Request, Response }  from "express";
 
 
 const app = express();
 
-const people:Ipeople[] =[
-  
-]
+const people:Ipeople[] =[]
+let gredeArr: Igrade[] =[]
+let taskArr: Itask[] = []
 
 
 const port = 3000;
@@ -42,30 +42,81 @@ people.push({
     })
 
 
-    app.put("/people/:id",(req,res)=>{
-        const people2 = req.body as Ipeople;
-        const { id } = req.params;
-       const peopleList = people.filter((people)=>{
-        return people.id !== id ;
-       })
+    // app.put("/people/:id",(req,res)=>{
+    //     const people2 = req.body as Ipeople;
+    //     const { id } = req.params;
+    //    const peopleList = people.filter((people)=>{
+    //     return people.id !== id ;
+    //    })
 
-       peopleList.push(people2)
-       people = peopleList;
-       return res.json({
-        message:"Done updating"
-       })
-    })
+    //    peopleList.push(people2)
+    //    people = peopleList;
+    //    return res.json({
+    //     message:"Done updating"
+    //    })
+    // })
 
-    app.delete('/people/:id',(req,res)=>{
+    // app.delete('/people/:id',(req,res)=>{
+    //     const { id } = req.params;
+    //    const peopleList = people1.filter((pp)=>{
+    //     return pp.id !== id ;
+    //    });
+    //    people = peopleList;
+    //    return res.json({
+    //     message:"people Deleted"
+    //    })
+    // })
+
+    app.get(`/task`, (req, res) => {
+        return res.json(taskArr);
+      });
+
+      app.post(`/task`, (req, res) => {
+        taskArr.push(req.body);
+        return res.json({
+          message: "task added !",
+        });
+      });
+
+      app.put(`/task/:id`, (req, res) => {
         const { id } = req.params;
-       const peopleList = people1.filter((pp)=>{
-        return pp.id !== id ;
-       });
-       people = peopleList;
-       return res.json({
-        message:"people Deleted"
-       })
-    })
+        const updateTask = req.body as Itask;
+        taskArr.map((task) => {
+          if (task.id === id) {
+            task.id = id;
+            task.description = updateTask.description;
+            task.title = updateTask.title;
+            task.status = updateTask.status;
+          }
+        });
+      
+        return res.json({
+          message: "task Updated !",
+        });
+      });
+
+      app.delete(`/task/:id`, (req, res) => {
+        const { id } = req.params;
+        const newArr = taskArr.filter((fil) => {
+          return fil.id !== id;
+        });
+      
+        taskArr = newArr;
+      
+        return res.json({
+          message: "task deleted !",
+        });
+      });
+
+      app.get(`/task/:id`, (req, res) => {
+        const { id } = req.params;
+      
+        taskArr.map((search) => {
+          if (search.id === id || search.title === id) {
+            return res.json(search);
+          }
+        });
+      });
 
 
 
--- src/types/people.ts
+++ src/types/people.ts
@@ -3,5 +3,14 @@ export interface Ipeople{
     name : string;
     grade : string;
    
-
 }
+export interface Igrade {
+    student: string;
+    grade: string;
+  }
+export interface Itask {
+    id: string;
+    title: string;
+    description: string;
+    status: boolean;
+  }
